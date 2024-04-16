06364d81e077e8860f385a0d4e06c481bc9badf9
Mir <156778551+mirstihl@users.noreply.github.com>
2024-04-16 19:18:48 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+
+
+    main()
+def main():
+
+
+import random
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    for item in data:
+    return data
+def generate_random_data():
