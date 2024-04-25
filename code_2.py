d99b159c1f847ffcbb34bb6b3dd3ee9aa18da1dd
Mir <156778551+mirstihl@users.noreply.github.com>
2024-04-25 17:19:09 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,17 +1,17 @@
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
     return data;
 
-function generateRandomData() {
-function main() {
-main();
-    const data = generateRandomData();
+
     });
-
-        console.log("Random Number: " + item);
-    const data = [];
 }
-    }
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-
     for (let i = 0; i < 10; i++) {
     data.forEach(item => {
+main();
+function generateRandomData() {
+function main() {
+    }
+
+}
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
